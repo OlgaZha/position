d7bcc08e623d0624f469aa388ff933c3fa913fd9
OlgaZha <zhaldakolga484@gmail.com>
2025-03-16 20:56:09 +0100

    hw done

---
+++ script.js
@@ -0,0 +1,15 @@
+class Node {
+    value
+    next
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
